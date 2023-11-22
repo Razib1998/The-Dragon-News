470bea14dd86f163fe736b68d563a0971da0d1d2
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2023-11-22 01:50:07 +0100

    Left Side Navbar are Competed

--- src/Pages/Shared/LeftSIdeNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSIdeNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( ()=>{
+        fetch("../../../../public/data/categories.json")
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
-        <div>
-            <h2 className="text-2xl">Left Nav</h2>
+        <div className="space-y-4">
+            <h2 className="text-xl ml-4 font-bold">ALl Categories</h2>
+            {
+                categories.map(category => <NavLink to={`/category/${category.id}`}
+                className= "block ml-4 text-xl font-semibold"
+                 key={category.id}>
+                {category.name}
+                </NavLink>)
+            }
         </div>
     );
 };
